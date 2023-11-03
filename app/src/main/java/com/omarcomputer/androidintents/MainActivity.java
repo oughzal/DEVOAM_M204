eb4f90fc0aa544ec1281f0ff1ddc5794ba11d804
Omar OUGHZAL <omar.oughzal@gmail.com>
2023-11-03 02:55:40 +0100

    Initial Commit

--- app/src/main/java/com/omarcomputer/androidintents/MainActivity.java
+++ app/src/main/java/com/omarcomputer/androidintents/MainActivity.java
@@ -2,28 +2,63 @@ package com.omarcomputer.androidintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements  View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    Button btnOpenActivity;
+    Button btnOpenActivity,btnSend,btnSendData;
+    EditText editMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnOpenActivity = findViewById(R.id.btnOpenActivity);
+        btnSend = findViewById(R.id.btnSend);
+        btnSendData = findViewById(R.id.btnSendData);
+        editMessage = findViewById(R.id.editMessage);
+
+        btnOpenActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO : créer une Intent pour ouvrir l'activity MainActivity2
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO : Créer une Intent pour envoyer un
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT,editMessage.getText().toString());
+                //TODO : créer une Intent pour choisir une application
+                Intent chooser = Intent.createChooser(intent,"Choisir une application");
+
+                startActivity(chooser);
+            }
+        });
+
+        btnSendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: envoyer le texte à afficher dans MainActivity2
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                intent.putExtra("title",editMessage.getText().toString());
+                startActivity(intent);
+
+            }
+        });
     }
 
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
 
-        // Cliquer sur le bouton open Activity
-        if(id==R.id.btnOpenActivity){
-            //TODO : créer une Intent pour ouvrire l'activity MainActivity2
-        }
-    }
+
 }
