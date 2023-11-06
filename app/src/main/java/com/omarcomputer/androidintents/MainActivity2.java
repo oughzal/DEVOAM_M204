c64698e42fa9cb2de44df492bf236c8cac8d1550
Omar OUGHZAL <omar.oughzal@gmail.com>
2023-11-06 23:51:38 +0100

    Initial Commit

--- app/src/main/java/com/omarcomputer/androidintents/MainActivity2.java
+++ app/src/main/java/com/omarcomputer/androidintents/MainActivity2.java
@@ -3,19 +3,31 @@ package com.omarcomputer.androidintents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
     TextView targetText;
+    ImageView targetImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         targetText = findViewById(R.id.targetText);
+        targetImage = findViewById(R.id.targetImage);
         Intent intent = getIntent();
-        if(intent !=null){
+        String type = intent.getType();
+        if(type ==null){
             targetText.setText(intent.getStringExtra("title"));
+        }else{
+            if ( Intent.ACTION_SEND.equals(intent.getAction()) && type.startsWith("image/")){
+                Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                if (imageUri != null) {
+                    targetImage.setImageURI(imageUri);
+                }
+            }
         }
 
     }
