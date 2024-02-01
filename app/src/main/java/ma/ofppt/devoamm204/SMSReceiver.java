1db9caac3d098eda64fd11b4c2e9a71b83d38916
Omar Oughzal <omar.oughzal@gmail.com>
2024-02-01 14:56:28 +0100

    Initial Commit

---
+++ app/src/main/java/ma/ofppt/devoamm204/SMSReceiver.java
@@ -0,0 +1,29 @@
+package ma.ofppt.devoamm204;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.util.Log;
+
+public class SMSReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if(intent.getAction() == Telephony.Sms.Intents.SMS_RECEIVED_ACTION){
+            Bundle bundle = intent.getExtras();
+            if (bundle != null) {
+                Object[] pdus = (Object[])bundle.get("pdus");
+                final SmsMessage[] messages = new SmsMessage[pdus.length];
+                for (int i = 0; i < pdus.length; i++) {
+                    messages[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                }
+                if (messages.length > -1) {
+                    Log.i("SMS_TAG", "Message recieved: " + messages[0].getMessageBody());
+                }
+            }
+            }
+        }
+    }
+}
